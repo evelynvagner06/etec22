d0b0cee8b98b8c185c376572b75fb7e0c535211f
evelynvagner06 <116077798+evelynvagner06@users.noreply.github.com>
2022-10-18 08:43:31 -0300

    Update calc.php

--- calc.php
+++ calc.php
@@ -1,11 +1,11 @@
 <?php
     $numero = $_POST['txtnumero'];
     Antecessor e Posterior
-            Antecessor e Posterior
-    $cont = 0;
-    while ($numero >= $divisor) {
-        $cont++;
+    Antecessor e Posterior
+    $Cont = 0;
+    while ($numero >= $Divisor) {
+        $Cont++;
         $numero -= 5;
     }
-    echo $cont;
+    echo $Cont;
 ?>
